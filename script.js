8f942dd427512d5229ec913ba72a91e395771e5d
J-Groesbeck <jacob.groesbeck@icloud.com>
2024-01-16 10:10:44 -0500

    Info now changes when you click the buttons

--- script.js
+++ script.js
@@ -3,3 +3,92 @@ const img1 = document.getElementById('img1')
 const img2 = document.getElementById('img2')
 const img3 = document.getElementById('img3')
 const img4 = document.getElementById('img4')
+
+function change1719() {
+    info.innerHTML = `<li>"Robinson Crusoe" is a novel written by Daniel Defoe, first published on April 25, 1719.</li>
+                <li>The novel is considered one of the earliest examples of realistic fiction and is often regarded as
+                    the first English novel.</li>
+                <li>It tells the story of Robinson Crusoe, a mariner who is shipwrecked and stranded on a deserted
+                    island for 28 years.</li>
+                <li>Crusoe's experiences on the island, including his struggle for survival, encounters with cannibals,
+                    and eventual companionship with Friday, make up the core of the narrative.</li>
+                <li>The novel is structured as a fictional autobiography, narrated by Robinson Crusoe himself.</li>
+                <li>"Robinson Crusoe" is known for its exploration of themes such as human resilience, self-reliance,
+                    and the consequences of colonialism.</li>
+                <li>The novel has been widely translated and adapted into various forms of literature, theater, and
+                    film, making it a classic in world literature.</li>
+                <li>Daniel Defoe's work laid the foundation for the development of the novel as a literary form and has
+                    had a lasting impact on storytelling.</li>`
+    img1.src = ''
+    img2.src = ''
+    img3.src = ''
+    img4.src = ''
+}
+
+function change1917() {
+    info.innerHTML = `<li>Ella Fitzgerald, a legendary jazz singer, was born on April 25, 1917, in Newport News, Virginia, USA.</li>
+                    <li>Often referred to as the "First Lady of Song" and the "Queen of Jazz," Fitzgerald had a remarkable vocal range and a unique ability to improvise.</li>
+                    <li>She began her career as a singer in the mid-1930s, gaining prominence with the Chick Webb Orchestra.</li>
+                    <li>Fitzgerald's breakthrough came with her hit recording of "A-Tisket, A-Tasket" in 1938, which showcased her vocal prowess and became a chart-topping success.</li>
+                    <li>Known for her pure tone, impeccable phrasing, and scat singing, Fitzgerald became one of the most influential jazz vocalists of her time.</li>
+                    <li>Over the course of her career, she collaborated with many jazz greats, including Duke Ellington, Count Basie, and Louis Armstrong.</li>
+                    <li>Fitzgerald's extensive discography includes numerous albums and iconic songs, making her a celebrated figure in the history of jazz music.</li>
+                    <li>She received numerous awards and honors, including 13 Grammy Awards, the Presidential Medal of Freedom, and the National Medal of Arts.</li>
+                    <li>Ella Fitzgerald's legacy extends beyond her incredible voice; she played a vital role in breaking racial barriers in the music industry during her time.</li>
+                    <li>Fitzgerald passed away on June 15, 1996, leaving behind a legacy that continues to inspire and influence generations of musicians.</li>`
+    img1.src = ''
+    img2.src = ''
+    img3.src = ''
+    img4.src = ''
+}
+
+function change1974() {
+    info.innerHTML = `<li>The overthrow of the fascist Estado Novo regime in Portugal occurred on April 25, 1974, in what is known as
+                      the Carnation Revolution.</li>
+                    <li>The revolution was a peaceful and largely bloodless military coup led by the Armed Forces Movement (Movimento das Forças Armadas or MFA), composed of young military officers dissatisfied with the authoritarian regime.</li>
+                    <li>The Estado Novo regime, led by Prime Minister Marcelo Caetano, had been in power for decades under the leadership of António de Oliveira Salazar before him.</li>
+                    <li>The coup was marked by the use of red carnations, symbolizing peace, as soldiers placed these flowers in the muzzles of their guns.</li>
+                    <li>The revolution was triggered by a combination of factors, including dissatisfaction with the colonial wars in Africa, economic difficulties, censorship, and restrictions on civil liberties.</li>
+                    <li>The military's rebellion quickly gained popular support among civilians, leading to widespread demonstrations and celebrations across Portugal.</li>
+                    <li>The overthrow of the Estado Novo marked the end of Portugal's long-standing authoritarian rule and paved the way for a transition to democracy.</li>
+                    <li>Following the revolution, political prisoners were released, censorship was lifted, and political exiles returned.</li>
+                    <li>Portugal underwent a process of decolonization, granting independence to its African colonies, including Mozambique, Angola, Cape Verde, Guinea-Bissau, and São Tomé and Príncipe.</li>
+                    <li>The Carnation Revolution is celebrated annually in Portugal as a national holiday, known as Freedom Day (Dia da Liberdade), commemorating the triumph of democracy and the end of dictatorship.</li>`
+    img1.src = ''
+    img2.src = ''
+    img3.src = ''
+    img4.src = ''
+}
+
+function change1990() {
+    info.innerHTML = `<li>The Hubble Space Telescope was launched into orbit on April 25, 1990, aboard the Space Shuttle Discovery
+                      (STS-31 mission).</li>
+                    <li>It was named after the astronomer Edwin Hubble and is a joint project between NASA and the European Space Agency (ESA).</li>
+                    <li>The telescope was deployed into low Earth orbit at an altitude of about 547 kilometers (340 miles).</li>
+                    <li>Hubble's primary mission is to observe distant galaxies, nebulae, and other celestial objects in visible, ultraviolet, and infrared light.</li>
+                    <li>The launch initially faced challenges, including a flaw in the telescope's primary mirror that affected its early images.</li>
+                    <li>A corrective optics package was installed during a servicing mission in 1993, significantly improving Hubble's image quality.</li>
+                    <li>Hubble has contributed immensely to our understanding of the universe, providing breathtaking images and valuable scientific data.</li>
+                    <li>It remains one of the most important and iconic astronomical observatories, contributing to numerous scientific discoveries and breakthroughs.</li>`
+    img1.src = ''
+    img2.src = ''
+    img3.src = ''
+    img4.src = ''
+}
+
+function change2015() {
+    info.innerHTML = `<li>A devastating earthquake with a magnitude of 7.8 struck Nepal on April 25, 2015.</li>
+                    <li>The earthquake's epicenter was near the capital city, Kathmandu, and it had a shallow depth, intensifying its impact on the region.</li>
+                    <li>The earthquake occurred as a result of the Indian and Eurasian tectonic plates colliding, causing significant seismic activity in the Himalayan region.</li>
+                    <li>The quake triggered widespread destruction, causing buildings, including historical structures and temples, to collapse in Kathmandu and surrounding areas.</li>
+                    <li>Villages and towns in rural areas were also severely affected, leading to extensive loss of life and infrastructure damage.</li>
+                    <li>The earthquake triggered avalanches on Mount Everest, resulting in fatalities among climbers at the Everest Base Camp.</li>
+                    <li>The disaster resulted in a humanitarian crisis, with a large number of people left homeless and in need of basic necessities.</li>
+                    <li>The international community responded with immediate aid and relief efforts, including search and rescue teams, medical assistance, and humanitarian aid.</li>
+                    <li>The earthquake's impact extended beyond Nepal, affecting neighboring countries like India, China, and Bangladesh.</li>
+                    <li>The aftermath prompted discussions about earthquake preparedness, building resilience, and the need for international collaboration in disaster response and recovery.</li>`
+    img1.src = ''
+    img2.src = ''
+    img3.src = ''
+    img4.src = ''
+}
