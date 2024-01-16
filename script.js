dd9dd184ae86d7176a03dd88e5100122db1f9e5b
J-Groesbeck <jacob.groesbeck@icloud.com>
2024-01-16 09:27:42 -0500

    Created Desktop Layout | Added Description | Added 1719 Info

---
+++ script.js
@@ -0,0 +1,5 @@
+const info = document.getElementById('info')
+const img1 = document.getElementById('img1')
+const img2 = document.getElementById('img2')
+const img3 = document.getElementById('img3')
+const img4 = document.getElementById('img4')
